711fba7fdd66c9f312c9ec548d960d342f9046c1
Joao Andre <joao.dasilva@avaiga.com>
2022-11-01 23:26:25 -0300

    chore: add rest tests

--- tests/test_taipy_community.py
+++ tests/test_taipy_community.py
@@ -1,17 +1,15 @@
 import datetime as dt
 from shared.config import *
 
-import taipy.core as tp
+import taipy as tp
 
 
-def test_submit_scenario_repository():
-        Config.configure_global_app(clean_entities_enabled=True)
-
+def test_submit_scenario_submit_success():
         scenario = tp.create_scenario(arima_scenario_config)
         tp.submit(scenario)
 
         assert scenario.forecast_values.read() is not None
         assert len(tp.get_pipelines()) == 1
-        assert len(tp.get_data_nodes()) == 4
         assert len(tp.get_tasks()) == 2
         assert len(tp.get_jobs()) == 2
+        assert len(tp.get_data_nodes()) == 4
