0c813c205038d1716e7ad771ab3c9e3dbf1487d4
Shiro <shiro@Shiros-MacBook-Pro.local>
2023-06-19 10:17:22 +0700

    fix path

--- tests/test_complex_application/utils/config_builders.py
+++ tests/test_complex_application/utils/config_builders.py
@@ -1,13 +1,31 @@
+# Copyright 2023 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
 import os
 import pathlib
 
-from taipy.config import Config, Scope, Frequency
+from taipy.config import Config, Frequency, Scope
 
 from .algos import *
 
 
 def build_complex_config():
-    csv_path_inp, excel_path_inp, csv_path_sum, excel_path_sum, excel_path_out, csv_path_out = build_complex_required_file_paths()
+    (
+        csv_path_inp,
+        excel_path_inp,
+        csv_path_sum,
+        excel_path_sum,
+        excel_path_out,
+        csv_path_out,
+    ) = build_complex_required_file_paths()
 
     inp_csv_dn_1 = Config.configure_csv_data_node("dn_csv_in_1", default_path=csv_path_inp)
     inp_csv_dn_2 = Config.configure_csv_data_node("dn_csv_in_2", default_path=csv_path_inp)
@@ -66,26 +84,21 @@ def build_complex_config():
 
 
 def build_complex_required_file_paths():
-    csv_path_inp = os.path.join(pathlib.Path(__file__).parent.resolve(),
-                                "../../shared_test_cases/data_sample/example.csv")
-    excel_path_inp = os.path.join(pathlib.Path(__file__).parent.resolve(),
-                                  "../../shared_test_cases/data_sample/example.xlsx")
+    csv_path_inp = "tests/shared_test_cases/data_sample/example.csv"
+    excel_path_inp = "tests/shared_test_cases/data_sample/example.xlsx"
 
-    csv_path_sum = os.path.join(pathlib.Path(__file__).parent.resolve(),
-                                "../../shared_test_cases/data_sample/sum.csv")
-    excel_path_sum = os.path.join(pathlib.Path(__file__).parent.resolve(),
-                                  "../../shared_test_cases/data_sample/sum.xlsx")
+    csv_path_sum = "tests/shared_test_cases/data_sample/sum.csv"
+    excel_path_sum = "tests/shared_test_cases/data_sample/sum.xlsx"
 
-    excel_path_out = os.path.join(pathlib.Path(__file__).parent.resolve(),
-                                  "../../shared_test_cases/data_sample/res.xlsx")
-    csv_path_out = os.path.join(pathlib.Path(__file__).parent.resolve(),
-                                "../../shared_test_cases/data_sample/res.csv")
+    excel_path_out = "tests/shared_test_cases/data_sample/res.xlsx"
+    csv_path_out = "tests/shared_test_cases/data_sample/res.csv"
     return csv_path_inp, excel_path_inp, csv_path_sum, excel_path_sum, excel_path_out, csv_path_out
 
 
 def build_churn_classification_required_file_paths():
-    csv_path_inp = os.path.join(pathlib.Path(__file__).parent.resolve(),
-                                "../../shared_test_cases/data_sample/churn.csv")
+    csv_path_inp = os.path.join(
+        pathlib.Path(__file__).parent.resolve(), "../../shared_test_cases/data_sample/churn.csv"
+    )
     return csv_path_inp
 
 
@@ -93,63 +106,50 @@ def build_churn_classification_config():
     csv_path_inp = build_churn_classification_required_file_paths()
 
     # path for csv and file_path for pickle
-    initial_dataset = Config.configure_data_node(id="initial_dataset",
-                                                 path=csv_path_inp,
-                                                 storage_type="csv",
-                                                 has_header=True)
+    initial_dataset = Config.configure_data_node(
+        id="initial_dataset", path=csv_path_inp, storage_type="csv", has_header=True
+    )
 
     date_cfg = Config.configure_data_node(id="date", default_data="None")
 
-    preprocessed_dataset = Config.configure_data_node(id="preprocessed_dataset",
-                                                      cacheable=True,
-                                                      validity_period=dt.timedelta(days=1))
+    preprocessed_dataset = Config.configure_data_node(
+        id="preprocessed_dataset", cacheable=True, validity_period=dt.timedelta(days=1)
+    )
 
     # the final datanode that contains the processed data
-    train_dataset = Config.configure_data_node(id="train_dataset",
-                                               cacheable=True,
-                                               validity_period=dt.timedelta(days=1))
+    train_dataset = Config.configure_data_node(id="train_dataset", cacheable=True, validity_period=dt.timedelta(days=1))
 
     # the final datanode that contains the processed data
-    trained_model = Config.configure_data_node(id="trained_model",
-                                               cacheable=True,
-                                               validity_period=dt.timedelta(days=1))
+    trained_model = Config.configure_data_node(id="trained_model", cacheable=True, validity_period=dt.timedelta(days=1))
 
-    trained_model_baseline = Config.configure_data_node(id="trained_model_baseline",
-                                                        cacheable=True,
-                                                        validity_period=dt.timedelta(days=1))
+    trained_model_baseline = Config.configure_data_node(
+        id="trained_model_baseline", cacheable=True, validity_period=dt.timedelta(days=1)
+    )
 
     # the final datanode that contains the processed data
-    test_dataset = Config.configure_data_node(id="test_dataset",
-                                              cacheable=True,
-                                              validity_period=dt.timedelta(days=1))
+    test_dataset = Config.configure_data_node(id="test_dataset", cacheable=True, validity_period=dt.timedelta(days=1))
 
-    forecast_dataset = Config.configure_data_node(id="forecast_dataset",
-                                                  scope=Scope.PIPELINE,
-                                                  cacheable=True,
-                                                  validity_period=dt.timedelta(days=1))
+    forecast_dataset = Config.configure_data_node(
+        id="forecast_dataset", scope=Scope.PIPELINE, cacheable=True, validity_period=dt.timedelta(days=1)
+    )
 
-    roc_data = Config.configure_data_node(id="roc_data",
-                                          scope=Scope.PIPELINE,
-                                          cacheable=True,
-                                          validity_period=dt.timedelta(days=1))
+    roc_data = Config.configure_data_node(
+        id="roc_data", scope=Scope.PIPELINE, cacheable=True, validity_period=dt.timedelta(days=1)
+    )
 
-    score_auc = Config.configure_data_node(id="score_auc",
-                                           scope=Scope.PIPELINE,
-                                           cacheable=True,
-                                           validity_period=dt.timedelta(days=1))
+    score_auc = Config.configure_data_node(
+        id="score_auc", scope=Scope.PIPELINE, cacheable=True, validity_period=dt.timedelta(days=1)
+    )
 
-    metrics = Config.configure_data_node(id="metrics",
-                                         scope=Scope.PIPELINE,
-                                         cacheable=True,
-                                         validity_period=dt.timedelta(days=1))
+    metrics = Config.configure_data_node(
+        id="metrics", scope=Scope.PIPELINE, cacheable=True, validity_period=dt.timedelta(days=1)
+    )
 
-    feature_importance_cfg = Config.configure_data_node(id="feature_importance",
-                                                        scope=Scope.PIPELINE)
+    feature_importance_cfg = Config.configure_data_node(id="feature_importance", scope=Scope.PIPELINE)
 
-    results = Config.configure_data_node(id="results",
-                                         scope=Scope.PIPELINE,
-                                         cacheable=True,
-                                         validity_period=dt.timedelta(days=1))
+    results = Config.configure_data_node(
+        id="results", scope=Scope.PIPELINE, cacheable=True, validity_period=dt.timedelta(days=1)
+    )
 
     ##############################################################################################################################
     # Creation of the tasks
@@ -159,89 +159,100 @@ def build_churn_classification_config():
     # and the output data node while executing the function
 
     # initial_dataset --> preprocess dataset --> preprocessed_dataset
-    task_preprocess_dataset = Config.configure_task(id="preprocess_dataset",
-                                                    input=[initial_dataset, date_cfg],
-                                                    function=preprocess_dataset,
-                                                    output=preprocessed_dataset)
+    task_preprocess_dataset = Config.configure_task(
+        id="preprocess_dataset",
+        input=[initial_dataset, date_cfg],
+        function=preprocess_dataset,
+        output=preprocessed_dataset,
+    )
 
     # preprocessed_dataset --> create train data --> train_dataset, test_dataset
-    task_create_train_test = Config.configure_task(id="create_train_and_test_data",
-                                                   input=preprocessed_dataset,
-                                                   function=create_train_test_data,
-                                                   output=[train_dataset, test_dataset])
+    task_create_train_test = Config.configure_task(
+        id="create_train_and_test_data",
+        input=preprocessed_dataset,
+        function=create_train_test_data,
+        output=[train_dataset, test_dataset],
+    )
 
     # train_dataset --> create train_model data --> trained_model
-    task_train_model = Config.configure_task(id="train_model",
-                                             input=train_dataset,
-                                             function=train_model,
-                                             output=[trained_model, feature_importance_cfg])
+    task_train_model = Config.configure_task(
+        id="train_model", input=train_dataset, function=train_model, output=[trained_model, feature_importance_cfg]
+    )
 
     # train_dataset --> create train_model data --> trained_model
-    task_train_model_baseline = Config.configure_task(id="train_model_baseline",
-                                                      input=train_dataset,
-                                                      function=train_model_baseline,
-                                                      output=[trained_model_baseline, feature_importance_cfg])
+    task_train_model_baseline = Config.configure_task(
+        id="train_model_baseline",
+        input=train_dataset,
+        function=train_model_baseline,
+        output=[trained_model_baseline, feature_importance_cfg],
+    )
 
     # test_dataset --> forecast --> forecast_dataset
-    task_forecast = Config.configure_task(id="predict_the_test_data",
-                                          input=[test_dataset, trained_model],
-                                          function=forecast,
-                                          output=forecast_dataset)
+    task_forecast = Config.configure_task(
+        id="predict_the_test_data", input=[test_dataset, trained_model], function=forecast, output=forecast_dataset
+    )
 
     # test_dataset --> forecast --> forecast_dataset
-    task_forecast_baseline = Config.configure_task(id="predict_of_baseline",
-                                                   input=[test_dataset, trained_model_baseline],
-                                                   function=forecast_baseline,
-                                                   output=forecast_dataset)
+    task_forecast_baseline = Config.configure_task(
+        id="predict_of_baseline",
+        input=[test_dataset, trained_model_baseline],
+        function=forecast_baseline,
+        output=forecast_dataset,
+    )
 
-    task_roc = Config.configure_task(id="task_roc",
-                                     input=[forecast_dataset, test_dataset],
-                                     function=roc_from_scratch,
-                                     output=[roc_data, score_auc])
+    task_roc = Config.configure_task(
+        id="task_roc", input=[forecast_dataset, test_dataset], function=roc_from_scratch, output=[roc_data, score_auc]
+    )
 
-    task_roc_baseline = Config.configure_task(id="task_roc_baseline",
-                                              input=[forecast_dataset, test_dataset],
-                                              function=roc_from_scratch,
-                                              output=[roc_data, score_auc])
+    task_roc_baseline = Config.configure_task(
+        id="task_roc_baseline",
+        input=[forecast_dataset, test_dataset],
+        function=roc_from_scratch,
+        output=[roc_data, score_auc],
+    )
 
-    task_create_metrics = Config.configure_task(id="task_create_metrics",
-                                                input=[forecast_dataset, test_dataset],
-                                                function=create_metrics,
-                                                output=metrics)
+    task_create_metrics = Config.configure_task(
+        id="task_create_metrics", input=[forecast_dataset, test_dataset], function=create_metrics, output=metrics
+    )
 
-    task_create_results = Config.configure_task(id="task_create_results",
-                                                input=[forecast_dataset, test_dataset],
-                                                function=create_results,
-                                                output=results)
+    task_create_results = Config.configure_task(
+        id="task_create_results", input=[forecast_dataset, test_dataset], function=create_results, output=results
+    )
 
     ##############################################################################################################################
     # Creation of the pipeline and the scenario
     ##############################################################################################################################
 
     # configuration of the pipeline and scenario
-    pipeline_preprocessing = Config.configure_pipeline(id="pipeline_preprocessing",
-                                                       task_configs=[task_preprocess_dataset,
-                                                                     task_create_train_test])
+    pipeline_preprocessing = Config.configure_pipeline(
+        id="pipeline_preprocessing", task_configs=[task_preprocess_dataset, task_create_train_test]
+    )
 
-    pipeline_train_baseline = Config.configure_pipeline(id="pipeline_train_baseline",
-                                                        task_configs=[task_train_model_baseline])
+    pipeline_train_baseline = Config.configure_pipeline(
+        id="pipeline_train_baseline", task_configs=[task_train_model_baseline]
+    )
     pipeline_train_model = Config.configure_pipeline(id="pipeline_train_model", task_configs=[task_train_model])
 
-    pipeline_model = Config.configure_pipeline(id="pipeline_model", task_configs=[task_forecast,
-                                                                                  task_roc,
-                                                                                  task_create_metrics,
-                                                                                  task_create_results])
+    pipeline_model = Config.configure_pipeline(
+        id="pipeline_model", task_configs=[task_forecast, task_roc, task_create_metrics, task_create_results]
+    )
 
-    pipeline_baseline = Config.configure_pipeline(id="pipeline_baseline", task_configs=[task_forecast_baseline,
-                                                                                        task_roc_baseline,
-                                                                                        task_create_metrics,
-                                                                                        task_create_results])
+    pipeline_baseline = Config.configure_pipeline(
+        id="pipeline_baseline",
+        task_configs=[task_forecast_baseline, task_roc_baseline, task_create_metrics, task_create_results],
+    )
 
     # the scenario will run the pipelines
-    scenario_cfg = Config.configure_scenario(id="churn_classification",
-                                             pipeline_configs=[pipeline_preprocessing,
-                                                               pipeline_train_baseline, pipeline_train_model,
-                                                               pipeline_model, pipeline_baseline],
-                                             frequency=Frequency.WEEKLY)
+    scenario_cfg = Config.configure_scenario(
+        id="churn_classification",
+        pipeline_configs=[
+            pipeline_preprocessing,
+            pipeline_train_baseline,
+            pipeline_train_model,
+            pipeline_model,
+            pipeline_baseline,
+        ],
+        frequency=Frequency.WEEKLY,
+    )
 
     return scenario_cfg
