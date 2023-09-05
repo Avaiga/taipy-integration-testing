84036507a43c2f35f449feedc9b50631551d6c7a
trgiangdo <dtr.giang.1299@gmail.com>
2023-09-05 14:25:46 +0700

    fix: failed templates tests due to new subparser under run subparser

--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,22 @@
+# Copyright 2023 Avaiga Private Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+
+from taipy._cli._base_cli import _CLI
+
+
+def clean_subparser():
+    if getattr(_CLI._parser, "_subparsers", None):
+        for choice in _CLI._parser._subparsers._group_actions[0].choices.values():
+            choice._subparsers = None
+
 
 def assert_true_after_time(assertion, msg=None, time=120):
     from datetime import datetime
@@ -5,7 +24,7 @@ def assert_true_after_time(assertion, msg=None, time=120):
 
     start = datetime.now()
     while (datetime.now() - start).seconds < time:
-        print(f"waiting {(datetime.now() - start).seconds} seconds...", end='\r')
+        print(f"waiting {(datetime.now() - start).seconds} seconds...", end="\r")
         try:
             if assertion():
                 print(f"waiting {(datetime.now() - start).seconds} seconds...")
