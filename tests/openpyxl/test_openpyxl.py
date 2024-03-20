4e8c4d675509cb51e5a5816ca5605fe374833eda
jrobinAV <jeanrobin.medori@avaiga.com>
2024-03-20 14:52:37 +0100

    Exhibit openpyxl error and add traces

--- tests/openpyxl/test_openpyxl.py
+++ tests/openpyxl/test_openpyxl.py
@@ -1,18 +1,27 @@
+import logging
+import openpyxl
+
+import os
+import pathlib
+import platform
+import shutil
+import sys
+
 def test_openpyxl():
-    import os
-    import pathlib
-    from openpyxl import load_workbook
-    import shutil
-
-    folder = pathlib.Path(__file__).parent.resolve()
-    path = folder / "openpyxl_test.xlsx"
-
     tmp_folder = pathlib.Path(__file__).parent.resolve() / "output_folder"
     os.makedirs(tmp_folder)
     tmp_path = tmp_folder / "output.xlsx"
-    shutil.copy(path, tmp_path)
+    shutil.copy(pathlib.Path(__file__).parent.resolve() / "openpyxl_test.xlsx", tmp_path)
 
-    excel_file = load_workbook(tmp_path)
+    excel_file = openpyxl.load_workbook(tmp_path)
     excel_file.close()
     if tmp_folder.exists():
-        shutil.rmtree(tmp_folder)
+        try:
+            shutil.rmtree(tmp_folder)  # Raises an error
+        except Exception as e:
+            logging.info("-------------------------------")
+            logging.info(f"{openpyxl.__version__=}")
+            logging.info(f"{sys.version=}")
+            logging.info(f"{sys.version_info=}")
+            logging.info(f"{platform.platform()}")
+            raise e
